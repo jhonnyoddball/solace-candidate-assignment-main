503fbadc92c31a5a5d6962107271bbbd61d7eabb
jhonnyoddball <jhonny.gonzalez@oddball.io>
2025-10-04 13:42:33 -0400

    Refactor table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,17 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import {
   Container,
   Typography,
   TextField,
   Button,
   Box,
-  Chip,
-  Stack,
-  Pagination,
-  CircularProgress
+  Pagination
 } from '@mui/material';
 import { Clear } from '@mui/icons-material';
 import { Advocate, PaginationInfo, ApiResponse } from '@/types';
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
+import AdvocatesTable from '@/components/advocates-table';
 
 export default function Home() {
   const [advocates, setAdvocates] = useState<Advocate[]>([]);
@@ -144,54 +114,7 @@ export default function Home() {
         </Typography>
       )}
 
-            <TableContainer component={Paper} sx={{ mt: 2 }}>
-        {loading ? (
-          <Box sx={{ display: 'flex', justifyContent: 'center', p: 4 }}>
-            <CircularProgress />
-          </Box>
-        ) : (
-          <Table sx={{ minWidth: 700 }} aria-label="advocates table">
-            <TableHead>
-              <TableRow>
-                <StyledTableCell>First Name</StyledTableCell>
-                <StyledTableCell>Last Name</StyledTableCell>
-                <StyledTableCell>City</StyledTableCell>
-                <StyledTableCell>Degree</StyledTableCell>
-                <StyledTableCell>Specialties</StyledTableCell>
-                <StyledTableCell align="right">Years of Experience</StyledTableCell>
-                <StyledTableCell align="right">Phone Number</StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {filteredAdvocates.map((advocate) => (
-                <StyledTableRow key={advocate.id}>
-                  <StyledTableCell component="th" scope="row">
-                    {advocate.firstName}
-                  </StyledTableCell>
-                  <StyledTableCell>{advocate.lastName}</StyledTableCell>
-                  <StyledTableCell>{advocate.city}</StyledTableCell>
-                  <StyledTableCell>{advocate.degree}</StyledTableCell>
-                  <StyledTableCell>
-                    <Stack direction="row" spacing={0.5} flexWrap="wrap">
-                      {advocate.specialties.map((specialty, index) => (
-                        <Chip
-                          key={index}
-                          label={specialty}
-                          size="small"
-                          variant="outlined"
-                          sx={{ mb: 0.5 }}
-                        />
-                      ))}
-                    </Stack>
-                  </StyledTableCell>
-                  <StyledTableCell align="right">{advocate.yearsOfExperience}</StyledTableCell>
-                  <StyledTableCell align="right">{advocate.phoneNumber}</StyledTableCell>
-                </StyledTableRow>
-              ))}
-            </TableBody>
-          </Table>
-        )}
-      </TableContainer>
+      <AdvocatesTable advocates={filteredAdvocates} loading={loading} />
 
       {/* Pagination Controls */}
       <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 3, gap: 2 }}>
